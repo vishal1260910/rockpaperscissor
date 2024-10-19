6dce30a50c7c9882a83bb857f53228a8b387be38
Vishal <kvishal126@gmail.com>
2024-10-19 17:13:01 +0530

    Added simple UI to the game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-alert("Hello Player!");
+// alert("Hello Player!");
 humanscore=0;
 computerscore=0;
+let humanchoice = "";
 
 function getComputerChoice() {
 	prob=Math.floor(Math.random()*3);
@@ -12,58 +13,168 @@ function getComputerChoice() {
 		return 'scissor'
 }
 
-function getHumanChoice(){
-	choice=prompt("Enter your choice rock, paper or scissor");
-	inchoice=choice.toLowerCase()
-	return(inchoice);
-}
+const choice = document.querySelector("#choice");
+const socreList = document.querySelector("p")
+const results = document.querySelector("#results");
+const btns = document.querySelector("#buttons");
+const finalScores = document.querySelector("#finalScores");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
+
+btns.addEventListener("click", function(e){
+	let target = e.target;
+	
+
+			switch(target.id){
+			case "rock":
+			humanchoice = "rock";
+			break;
+
+			case "paper":
+			humanchoice = "paper";
+			break;
+
+			case "scissor":
+			humanchoice = "scissor";	
+			break;
+		}
+
+		playRound();
+
+
+		if(humanscore===5){
+			const Result = document.createElement("p");
+			
+			Result.textContent = "You Win";
+			
+			socreList.appendChild(Result);
+			displayScores();
+			disableButtons();
+			// resetGame();
+		
+		}
+
+		if (computerscore===5){
+			const Result = document.createElement("p");
+			
+			Result.textContent = "Computer Won"
+
+			socreList.appendChild(Result);
+			displayScores();
+			disableButtons();
+
+	}
+			
+});
 
 function playRound(){
-	humanchoice=getHumanChoice();
 	computerchoice=getComputerChoice();
+
+	   socreList.textContent = "";
 	if (humanchoice === 'rock' && computerchoice === 'paper'){
 		computerscore++;
-		alert("You lose");
+			const outcome = document.createElement("p");
+			showcompChoice()
+
+			outcome.textContent = "You Lost this round";
+			socreList.appendChild(outcome);
+		
 	}
 
 	else if (humanchoice === 'rock' && computerchoice === 'scissor'){
 		humanscore++;
-		alert("You win!");
+		const outcome = document.createElement("p");
+		showcompChoice();
+
+			outcome.textContent = "You won this round";
+			socreList.appendChild(outcome);
 	}
 
 
 	else if (humanchoice === 'paper' && computerchoice === 'rock'){
 		humanscore++;
-		alert("You win");
+		const outcome = document.createElement("p");
+		showcompChoice();
+
+			outcome.textContent = "You won this round";
+			socreList.appendChild(outcome);
+	
 	}
 
 	else if (humanchoice === 'paper' && computerchoice === 'scissor'){
 		computerscore++;
-		alert("You lose");
+	const outcome = document.createElement("p");
+	showcompChoice();
+
+			outcome.textContent = "You Lost this round";
+			socreList.appendChild(outcome);
 	}
 
 	else if (humanchoice === 'scissor' && computerchoice === 'paper'){
 		humanscore++;
-		alert("You win");
+		const outcome = document.createElement("p");
+		showcompChoice();
+
+			outcome.textContent = "You won this round";
+			socreList.appendChild(outcome);
 	}
 
 	else if (humanchoice === 'scissor' && computerchoice === 'rock'){
 		computerscore++;
-		alert("You lose");
+		const outcome = document.createElement("p");
+		showcompChoice();
+
+			outcome.textContent = "You Lost this round";
+			socreList.appendChild(outcome);
+		
 	}
 
 	else if (humanchoice ===  computerchoice ){
-		alert("Tie");
+		const outcome = document.createElement("p");
+		showcompChoice();
+
+			outcome.textContent = "It's a tie";
+			socreList.appendChild(outcome);
+
+
+
+			
 	}
-
-
-
-confirm( "Computer Score = " + " " + computerscore);
-confirm("Your Score" + " " + humanscore);
 }
 
-// for(let i =0; i<5; i++){
-// playRound();
-// }
+function displayScores(){
+	finalScores.textContent = "Your final score is  " +  (+humanscore) +" " + "Computer's Final score is  "+ (+computerscore);
+}
 
-alert("Your final score:  " + humanscore +" " + "\nComputer final score: "+ computerscore)
+const resetbtn = document.createElement("button");
+document.body.appendChild(resetbtn);
+resetbtn.textContent= "Reset Game";
+resetbtn.addEventListener("click", resetGame);
+
+function resetGame(){
+	humanscore= 0;
+	computerscore = 0;
+	socreList.textContent="";
+	finalScores.textContent = "";
+	choice.textContent = "";
+
+	rock.disabled =false;
+	paper.disabled=false;
+	scissor.disabled =false;
+}
+
+
+function disableButtons(){
+	rock.disabled = true;
+	scissor.disabled=true;
+	paper.disabled = true;
+}
+
+function showcompChoice(){
+choice.textContent="Computer Chose " + computerchoice  +" " + " You chose" +" "+  humanchoice;
+}
+
+
+
+
